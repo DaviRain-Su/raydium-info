dee0b6967c8edde3dd387b5856b23111fadbb25b
Davirain <davirain.yin@gmail.com>
2024-07-30 18:29:44 +0800

    init

---
+++ plasmo.config.ts
@@ -0,0 +1,10 @@
+import type { PlasmoConfig } from "plasmo"
+
+export const config: PlasmoConfig = {
+  manifest: {
+    name: "Raydium Info",
+    description: "Browser extension for Raydium information",
+    version: "0.0.1",
+    host_permissions: ["https://api-v3.raydium.io/*"]
+  }
+}
